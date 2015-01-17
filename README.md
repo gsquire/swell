3fbe4e32268324a91adceacd485a59853af356ae
Garrett Squire <garrettsquire@gmail.com>
2015-01-17 12:02:52 -0800

    added a dependency using Cargo and started request parsing

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ zinc
 ====
 
 A web server in Rust.
+
+This is my senior project at Cal Poly. It will be under constant construction
+until June when I graduate. I will do my best to keep up with the Rust releases. I am no expert and this is a huge learning experience for me and should not be used in production in any form.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+extern crate hyper;
+
 use std::io::{TcpListener, TcpStream};
 use std::io::{Acceptor, Listener};
-use std::io::net::ip::{Ipv4Addr, SocketAddr};
-use std::thread::Thread;
+
+use hyper::http::read_request_line;
 
 fn handle_client(mut stream: TcpStream) {
+    match stream.read_to_string() {
+        Err(e) => { println!("error reading request: {}", e); }
+        Ok(req) => { println!("the request is: {}", req); }
+    }
     let resp = b"HTTP/1.1 200 OK\r\nContent-Type: text/html;\r\n\r\nHello!\n";
     match stream.write(resp) {
         Err(e) => { println!("Error writing response: {}", e); }
@@ -12,16 +18,15 @@ fn handle_client(mut stream: TcpStream) {
 }
 
 fn main() {
-    let addr = SocketAddr { ip: Ipv4Addr(127, 0, 0, 1), port: 42007 };
-    let listener = TcpListener::bind(addr).unwrap();
+    let listener = TcpListener::bind("127.0.0.1:42007");
 
     let mut acceptor = listener.listen();
-    println!("Listening on port {}", addr.port);
 
     for stream in acceptor.incoming() {
-        Thread::spawn(move || {
-            handle_client(stream.unwrap())
-        });
+        match stream {
+            Err(e) => { println!("Error in stream: {}", e); }
+            Ok(stream) => { handle_client(stream) }
+        }
     }
 
     drop(acceptor);
