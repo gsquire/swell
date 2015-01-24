287aa7967cdf056d4e6955c3bac7f5b2d1fa0b0c
Garrett Squire <garrettsquire@gmail.com>
2015-01-24 15:20:34 -0800

    added some more file handling capability

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate hyper;
 #[macro_use] extern crate log;
 
 use std::io::File;
+use std::io::fs::PathExtensions;
 use std::io::net::ip::Ipv4Addr;
 
 use hyper::Get;
@@ -20,18 +21,37 @@ macro_rules! try_return(
     }}
 );
 
-fn default(req: Request, mut res: Response) {
+fn send_file(path: &str, mut res: Response) {
+    let root = "/Users/gsquire/poly/senior_project/html";
+    let out: String;
+
+    if path == "/" {
+        out = try_return!(File::open(&Path::new(root.to_string() + "/index.html")).read_to_string());
+    } else {
+        // Let us check if the path actually exists.
+        let path = Path::new(root.to_string() + path);
+        if path.exists() {
+            out = try_return!(File::open(&path).read_to_string());
+        } else {
+            // Return a 404.
+            *res.status_mut() = hyper::NotFound;
+            try_return!(res.start().and_then(|res| res.end()));
+            return;
+        }
+    }
+
+    res.headers_mut().set(ContentLength(out.len() as u64));
+    let mut res = try_return!(res.start());
+    try_return!(res.write_str(out.as_slice()));
+    try_return!(res.end());
+}
+
+fn base(req: Request, mut res: Response) {
     match req.uri {
         AbsolutePath(ref path) => match (&req.method, path.as_slice()) {
             (&Get, _) => {
-                if path.as_slice() == "/" {
-                    let out = try_return!(File::open(&Path::new("/Users/gsquire/poly/senior_project/html/index.html")).read_to_string());
-                    res.headers_mut().set(ContentLength(out.len() as u64));
-                    let mut res = try_return!(res.start());
-                    try_return!(res.write_str(out.as_slice()));
-                    try_return!(res.end());
-                    return;
-                }
+                send_file(path.as_slice(), res);
+                return;
             },
             _ => {
                 *res.status_mut() = hyper::NotFound;
@@ -48,7 +68,7 @@ fn default(req: Request, mut res: Response) {
 
 fn main() {
     let server = Server::http(Ipv4Addr(127, 0, 0, 1), 42007);
-    let mut listener = server.listen(default).unwrap();
+    let mut listener = server.listen(base).unwrap();
     println!("Listening on port 42007...");
     listener.await();
 }
